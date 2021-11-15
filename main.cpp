d89a84f30e60d8c0f9b028614aff782d83389810
kanade-k-1228 <kanade.k.1228@gmail.com>
2021-11-16 03:30:49 +0900

    Commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+// #define IS_FULL_SCREEN
+
 #include <GL/glut.h>
 #include <display.hpp>
 #include <initialize.hpp>
@@ -15,8 +17,12 @@ int main(int argc, char* argv[]) {
   glutInitWindowPosition(0, 0);                  // ウィンドウの場所（モニタ座標上で）
   glutInitWindowSize(1920, 1080);                // ウィンドウサイズ（ピクセル単位）
   glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE);  // デフォルト値
-  glutCreateWindow("バーチャル旋盤");            // ウィンドウ生成
-  // glutEnterGameMode();                           // フルスクリーンモード
+
+#ifdef IS_FULL_SCREEN
+  glutEnterGameMode();  // フルスクリーンモード
+#else
+  glutCreateWindow("バーチャル旋盤");  // ウィンドウ生成
+#endif
 
   // コールバック関数登録
   glutDisplayFunc(display);               // ディスプレイの再描画
--- src/global/hpp/global.hpp
+++ src/global/hpp/global.hpp
@@ -2,16 +2,18 @@
 #include <cylinder.hpp>
 
 namespace Global {
-extern Cylinder work;
+// ウィンドウ
 extern Point2D<int> window;
+// カメラ
+extern Point3D<int> camera_position;
+extern Point3D<int> camera_target;
+extern Point3D<int> camera_posture;
+extern int camera_viewing_angle;
+extern int camera_render_min;
+extern int camera_render_max;
+// マウス
 extern Point2D<int> current_mouse;
 extern Point2D<int> prev_mouse;
-
-// カメラ
-extern Point3D<int> camera_position;  // 位置
-extern Point3D<int> camera_target;    // 注視点
-extern Point3D<int> camera_posture;   // 姿勢
-extern int camera_viewing_angle;      // 視野角
-extern int camera_render_min;         // 最小描画距離
-extern int camera_render_max;         // 最大描画距離
+// オブジェクト
+extern Cylinder work;
 }  // namespace Global
