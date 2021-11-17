b9e56a072500f7ac6619c7ab4cf78fe045ba379f
kanade-k-1228 <kanade.k.1228@gmail.com>
2021-11-17 14:37:24 +0900

    Fix directory structure

--- src/callback/cpp/keyboard.cpp
+++ src/callback/cpp/keyboard.cpp
@@ -4,7 +4,7 @@ void keyboard(const unsigned char key, const int x, const int y) {
   std::cout << "key\t" << key << std::endl;
   if(key == 27) exit(0);  // ESCキーで終了
   if(key == ' ') {
-    Global::work.reset();
+    Global::init();
     Global::draw();
   }
 }
